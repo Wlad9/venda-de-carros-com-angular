da69647781235b90a530ea35cfaddba819ddd843
wlad9 <zav8b9@gmail.com>
2022-09-27 11:03:45 -0300

    Primeiro commit- repositório criado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'venda-de-carros-com-angular';
+  title = 'Bem vindo ao curso de Frontend com Angular';
 }
